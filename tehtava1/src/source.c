d2bf965102d030695c2fc2a9d8511e4aee1a4462
Noora Imberg <noppakotilo@gmail.com>
2015-05-20 10:20:16 +0300

    tehtava1

---
+++ tehtava1/src/source.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include "source.h"
+
+/* Exercise 1.1 */
+void print_lines(void)
+{
+    /*BEGIN SOLUTION*/
+    printf("Eka\nToka\nKolmas\n");
+    /*END SOLUTION*/
+}
+
